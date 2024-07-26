0a1459b14ab27770c7cdf4611559ea57bbbe51df
taddjv <97317254+taddjv@users.noreply.github.com>
2024-07-26 17:34:56 +0000

    nav work

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,62 +9,31 @@ import {
 } from "framer-motion";
 
 function Navigation() {
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 2]);
-
-  const { scrollY } = useScroll();
-  useEffect(() => {
-    const unsub = scrollY.on("change", (latest) => console.log(latest));
-    // console.log(scrolly);
-    return () => unsub;
-  }, [scrollY]);
   return (
     <>
       <header>
-        <motion.div
-          className="social"
-          initial={{ y: -1000 }}
-          animate={{ y: 1 }}
-          transition={{
-            type: "spring",
-            stiffness: 75,
-            damping: 20,
-            delay: 0.5,
-          }}
-        ></motion.div>
-        <motion.a
-          href="/"
-          className="logo"
-          initial={{ scale: 0, y: -1000 }}
-          animate={{ rotate: 360, scale: 1, y: 0 }}
-          transition={{
-            type: "spring",
-            stiffness: 75,
-            damping: 20,
-          }}
-        >
-          {/* <img className="logoImage" src={logo} alt="Logo" /> */}
-        </motion.a>
-        <motion.div
-          className="links"
-          initial={{ scale: 0, x: -1000 }}
-          animate={{ scale: 1, x: 1 }}
-          transition={{
-            type: "spring",
-            stiffness: 75,
-            damping: 20,
-            delay: 0.5,
-          }}
-          style={{
-            scale,
-          }}
-        >
-          <a href="#a-propos">À propos</a>
-          <a href="#notre-equipe">Notre équipe</a>
-          <a href="#nos-services">Nos services</a>
-          {/* <a href="/">Blog</a> */}
-          <a href="/">Nous joindre</a>
-        </motion.div>
+        <div className="links">
+          <div className="links-box">
+            <div className="lb-box"></div>
+            <a href="#a-propos">À propos</a>
+          </div>
+          {/* <a href="#a-propos">À propos</a> */}
+          <div className="links-line"></div>
+          <div className="links-box">
+            <div className="lb-box"></div>
+            <a href="#nos-services">Mes services</a>
+          </div>
+          <div className="links-line"></div>
+          <div className="links-box">
+            <div className="lb-box"></div>
+            <a href="/">Blog</a>
+          </div>
+          <div className="links-line"></div>
+          <div className="links-box">
+            <div className="lb-box"></div>
+            <a href="/">Me joindre</a>
+          </div>
+        </div>
       </header>
     </>
   );
--- src/components/NosServices.jsx
+++ src/components/NosServices.jsx
@@ -1,75 +1,101 @@
 import React, { useEffect } from "react";
 import "../style/nosServices.css";
-import insurance from "../assets/insurance.svg";
-import savings from "../assets/savings.svg";
-import investing from "../assets/investing.svg";
+import insurance from "../assets/insurance.jpg";
+import savings from "../assets/savings.jpg";
+import investing from "../assets/investing.jpg";
 import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 function NosServices() {
-  const squareVariants = {
-    visible: { opacity: 1, transition: { duration: 2, delay: 0.25 } },
-    hidden: { opacity: 0 },
-  };
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    } else {
-      controls.start("hidden");
-    }
-  }, [controls, inView]);
   return (
-    <>
-      <motion.section
-        id="nos-services"
-        className="nos-services"
-        ref={ref}
-        animate={controls}
-        initial="hidden"
-        variants={squareVariants}
-      >
-        <div className="apt-con">
-          <span className="ap-title">Nos Services</span>
-        </div>
-
-        <div className="cards-container">
-          <div className="ns-card">
-            <img className="nsc-logo" src={insurance} />
-            <div className="nsc-title">Assurance</div>
-            <div className="nsc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-              repudiandae similique unde deserunt nihil recusandae ipsum,
-              consequatur porro pariatur placeat autem temporibus doloremque at.
-              Dicta odio incidunt cupiditate nesciunt quae?
-            </div>
-          </div>
-          <div className="ns-card">
-            <img className="nsc-logo" src={savings} />
-            <div className="nsc-title">Épargne</div>
-            <div className="nsc-text">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-              repudiandae similique unde deserunt nihil recusandae ipsum,
-              consequatur porro pariatur placeat autem temporibus doloremque at.
-              Dicta odio incidunt cupiditate nesciunt quae?
-            </div>
-          </div>
-          <div className="ns-card">
-            <img className="nsc-logo" src={investing} />
-            <div className="nsc-title">Placements</div>
-            <div className="nsc-text">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-              repudiandae similique unde deserunt nihil recusandae ipsum,
-              consequatur porro pariatur placeat autem temporibus doloremque at.
-              Dicta odio incidunt cupiditate nesciunt quae?
-            </div>
+    <section className="nos-services">
+      {/* <div className="ns-service"> */}
+      <div className="ns-service">
+        <div className="nss-left">
+          <div className="ap-title">Assurance</div>
+          <div className="nsl-desc">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+            repudiandae similique unde deserunt nihil recusandae ipsum,
+            consequatur porro pariatur placeat autem temporibus doloremque at.
+            Dicta odio incidunt cupiditate nesciunt quae?
           </div>
         </div>
-      </motion.section>
-    </>
+        <div className="nss-right">
+          <img src={insurance} className="nss-right-img" />
+        </div>
+        {/* </div> */}
+        {/* <div className="ns-service"> */}
+        <div className="nss-right">
+          <img src={savings} className="nss-right-img" />
+        </div>
+        <div className="nss-left">
+          <div className="ap-title">Épargne</div>
+          <div className="nsl-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+            repudiandae similique unde deserunt nihil recusandae ipsum,
+            consequatur porro pariatur placeat autem temporibus doloremque at.
+            Dicta odio incidunt cupiditate nesciunt quae?
+          </div>
+        </div>
+        {/* </div> */}
+        {/* <div className="ns-service"> */}
+        <div className="nss-left">
+          <div className="ap-title">Placements</div>
+          <div className="nsl-desc">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+            repudiandae similique unde deserunt nihil recusandae ipsum,
+            consequatur porro pariatur placeat autem temporibus doloremque at.
+            Dicta odio incidunt cupiditate nesciunt quae?
+          </div>
+        </div>
+        <div className="nss-right">
+          <img src={investing} className="nss-right-img" />
+        </div>
+      </div>
+      {/* <div className="nss-left">
+          <div className="nsl-title">Assurance</div>
+          <div className="nsl-desc">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+            repudiandae similique unde deserunt nihil recusandae ipsum,
+            consequatur porro pariatur placeat autem temporibus doloremque at.
+            Dicta odio incidunt cupiditate nesciunt quae?
+          </div>
+        </div>
+        <div className="nss-right">
+          <img src={insurance} className="nss-right-img" />
+        </div>
+      </div>
+      <div className="ns-service">
+        <div className="nss-left">
+          <div className="nsl-title">Épargne</div>
+          <div className="nsl-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+            repudiandae similique unde deserunt nihil recusandae ipsum,
+            consequatur porro pariatur placeat autem temporibus doloremque at.
+            Dicta odio incidunt cupiditate nesciunt quae?
+          </div>
+        </div>
+        <img src={savings} className="nss-right" />
+      </div>
+      <div className="ns-service">
+        <div className="nss-left">
+          <div className="nsl-title">Placements</div>
+          <div className="nsl-desc">
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+            repudiandae similique unde deserunt nihil recusandae ipsum,
+            consequatur porro pariatur placeat autem temporibus doloremque at.
+            Dicta odio incidunt cupiditate nesciunt quae?
+          </div>
+        </div> 
+        <img src={investing} className="nss-right" />
+        </div>
+  */}
+    </section>
   );
 }
+
 export default NosServices;
